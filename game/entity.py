aa3f10ec77342a4af76f2b60193eda87323ec649
lance-drake <dra15006@byui.edu>
2021-03-25 16:20:17 -0600

    spell check

--- game/entity.py
+++ game/entity.py
@@ -3,5 +3,5 @@ import arcade
 class Entity(arcade.Sprite):
     def __init__(self):
         super().__init__()
-        self.center_x = pass
-        self.center_y = pass
+        self.center_x = 'pass'
+        self.center_y = 'pass'
