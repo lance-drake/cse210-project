4ebafa10fa4bb9f716f4cdda8b355eb78a72ebdb
lance-drake <dra15006@byui.edu>
2021-03-31 19:32:07 -0600

    Entity Class

--- game/entity.py
+++ game/entity.py
@@ -1,7 +1,8 @@
 import arcade
+import Maze.main
 
 class Entity(arcade.Sprite):
     def __init__(self):
-        super().__init__()
-        self.center_x = 'pass'
-        self.center_y = 'pass'
+        super().__init__('https://arcade.academy/resources/images/enemies/fishPink.png')
+        self.center_x = 0
+        self.center_y = int(Maze.main.SCREEN_HEIGHT/2)
