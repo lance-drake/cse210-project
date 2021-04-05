ab3c73db183a3b9e836877d601f6e652260644da
MagicWarrior494 <58752281+MagicWarrior494@users.noreply.github.com>
2021-04-05 13:45:16 -0600

    1.0

--- handsies/game/handle_collisions_action.py
+++ handsies/game/handle_collisions_action.py
@@ -13,8 +13,37 @@ class HandleCollisionsAction(Action):
         player2 = cast["players"][1]
         
         if player1.get_a_hand().collides_with_sprite(player2.get_a_hand()):
-            print("player1's a collided with player2's a!")
-        
+            if(player1.attacker):
+                player1.points += 1
+            elif(player2.attacker):
+                player2.points += 1
+            player1.resetPosition()
+            player2.resetPosition()
         if player1.get_b_hand().collides_with_sprite(player2.get_b_hand()):
-            print("player1's b collided with player2's b!")
-        
+            if(player1.attacker):
+                player1.points += 1
+            elif(player2.attacker):
+                player2.points += 1
+            player1.resetPosition()
+            player2.resetPosition()
+
+        if(player1.attacker):
+            if(player2.get_a_hand().passedLine and player1.get_a_hand()._key_down):
+                self.switchAttacker(cast)
+            if(player2.get_b_hand().passedLine and player1.get_b_hand()._key_down):
+                self.switchAttacker(cast)
+
+        elif(player2.attacker):
+            if(player1.get_a_hand().passedLine and player2.get_a_hand()._key_down):
+                self.switchAttacker(cast)
+            if(player1.get_b_hand().passedLine and player2.get_b_hand()._key_down):
+                self.switchAttacker(cast)
+
+    def switchAttacker(self, cast):
+        player1 = cast["players"][0]
+        player2 = cast["players"][1]
+
+        player1.attacker = not player1.attacker
+        player2.attacker = not player2.attacker
+        player1.resetPosition()
+        player2.resetPosition()
--- handsies/game/player.py
+++ handsies/game/player.py
@@ -2,18 +2,39 @@ import arcade
 from game.constants import *
 from game.hand import Hand
 
-
 class Player:
-    def __init__(self, direction):
+    def __init__(self, direction, attacker):
         if direction not in ["left", "right"]:
             raise ValueError("position must be 'left' or 'right'")
         self._a_hand = None
         self._b_hand = None
+        self.attacker = attacker
+        self.direction = direction
         self._setup(direction)
+        self.points = 0
+        self.resetting = False
+        self.line = None
         
+    
+    def resetPosition(self):
+        self._a_hand.resetPosition(self.attacker)
+        self._b_hand.resetPosition(self.attacker)
+
     def draw(self):
         self._a_hand.draw()
         self._b_hand.draw()
+        if(not self.attacker):
+            if(self.direction == "left"):
+                arcade.draw_line((MAX_X/2) - 206,0 , (MAX_X/2) - 206, MAX_Y, arcade.color.YELLOW)
+                
+            elif(self.direction == "right"):
+                arcade.draw_line((MAX_X/2) + 206, 0, (MAX_X/2) + 206, MAX_Y, arcade.color.YELLOW)
+        if(self.direction == "right"):
+            arcade.draw_text(str(self.points), 0,MAX_Y-20,arcade.color.BLUE)
+        elif(self.direction == "left"):
+            arcade.draw_text(str(self.points), MAX_X-20,MAX_Y-20,arcade.color.BLUE)
+           
+            
 
     def get_a_hand(self):
         return self._a_hand
@@ -22,23 +43,19 @@ class Player:
         return self._b_hand
         
     def update(self):
-        self._a_hand.update()
-        self._b_hand.update()
+        self._a_hand.update(self.attacker)
+        self._b_hand.update(self.attacker)
     
     def _setup(self, direction):
         if direction == "left":
-            self._a_hand = Hand(RIGHT_FACING_A_IMAGE,1, 200,0,600)
-            self._a_hand.left = 0
+            self._a_hand = Hand(RIGHT_FACING_A_IMAGE, 1, 0, (MAX_X/2) + 106, self.attacker)
             self._a_hand.center_y = A_HAND_Y
-            self._b_hand = Hand(RIGHT_FACING_B_IMAGE, -1,200,0,200)
-            self._b_hand.left = 200
+            self._b_hand = Hand(RIGHT_FACING_B_IMAGE, 1, 0, (MAX_X/2) + 106, self.attacker)
             self._b_hand.center_y = B_HAND_Y
             
         elif direction == "right":
-            self._a_hand = Hand(LEFT_FACING_A_IMAGE, -1, 600,400,900)
-            self._a_hand.right = (MAX_X - 200)
+            self._a_hand = Hand(LEFT_FACING_A_IMAGE, -1, (MAX_X/2) - 106, MAX_X, self.attacker)
             self._a_hand.center_y = A_HAND_Y
-            self._b_hand = Hand(LEFT_FACING_B_IMAGE, 1,800,400,800)
-            self._b_hand.right = MAX_X
+            self._b_hand = Hand(LEFT_FACING_B_IMAGE, -1, (MAX_X/2) - 106, MAX_X, self.attacker)
             self._b_hand.center_y = B_HAND_Y
         
